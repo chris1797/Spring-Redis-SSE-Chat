db51d0b9d5b809cfb8857b61b3eec0af9ab0cf43
chris1797 <jaehun6004@gmail.com>
2025-02-04 00:29:18 +0900

    feat: StompHandler message

--- src/main/java/com/base/demo/config/StompHandler.java
+++ src/main/java/com/base/demo/config/StompHandler.java
@@ -1,10 +1,27 @@
 package com.base.demo.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Slf4j
+@Component
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
+
+
+    /**
+     * 메세지 전달 전 처리
+     * @param message 메시지
+     * @param channel 채널
+     * @return Message 메시지
+     */
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        log.info("preSend");
+        return message;
+    }
 }
