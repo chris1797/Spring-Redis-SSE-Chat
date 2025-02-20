7e89e3931b95e3db24ae733c1e200e82130b06e4
chris1797 <jaehun6004@gmail.com>
2025-02-21 02:12:32 +0900

    Feat: RedisChatSubscriber 내부 구현

--- src/main/java/com/base/demo/message/service/subscribe/RedisChatSubscriber.java
+++ src/main/java/com/base/demo/message/service/subscribe/RedisChatSubscriber.java
@@ -1,16 +1,41 @@
 package com.base.demo.message.service.subscribe;
 
+import com.base.demo.message.entity.MessageContent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+
+
+/*
+Publish 된 메시지를 받아서 처리하는 클래스, pub된 메세지를 해당 클래스에서 받아 로깅, 비즈니스 로직 처리 등을 하는 역할.
+ */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisChatSubscriber implements MessageListener {
 
+    private final ObjectMapper objectMapper;
+    private final SimpMessageSendingOperations stompSendingOperations;
+
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
+        try {
+            String messageBody = new String(message.getBody(), StandardCharsets.UTF_8);
+            MessageContent messageContent = objectMapper.readValue(messageBody, MessageContent.class);
+            log.info("Message received: {}", messageContent);
+
+            stompSendingOperations.convertAndSend("/topic/messages/1", messageContent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
